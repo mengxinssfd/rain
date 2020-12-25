b647ca402675699d51b0532535f06ec20d0aac2b
dengyonghua <xinzon32177@163.com>
2020-12-25 13:30:32 +0800

    ref

--- index.ts
+++ index.ts
@@ -1,30 +1,29 @@
 class Rain {
-    private context: CanvasRenderingContext2D;
+  private readonly context: CanvasRenderingContext2D;
 
+  constructor(private width: number, private height: number) {
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    document.body.appendChild(canvas);
 
-    constructor(private width: number, private height: number) {
-        const canvas: HTMLCanvasElement = document.createElement("canvas");
-        canvas.width = width;
-        canvas.height = height;
-        document.body.appendChild(canvas);
-
-        if (!canvas.getContext) {
-            throw new Error("canvas unsupported");
-        }
-
-        this.context = canvas.getContext("2d");
-        this.setBg();
+    if (!canvas.getContext) {
+      throw new Error("canvas unsupported");
     }
 
-    setBg(color = "black") {
-        const ctx = this.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(0, 0, this.width, this.height);
-    }
+    this.context = canvas.getContext("2d");
+    this.setBg();
+  }
 
-    clearCanvas() {
-        this.context.clearRect(0, 0, this.width, this.height);
-    }
+  setBg(color = "black") {
+    const ctx = this.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, this.width, this.height);
+  }
+
+  clearCanvas() {
+    this.context.clearRect(0, 0, this.width, this.height);
+  }
 }
 
 const screenWidth = window.innerWidth;
